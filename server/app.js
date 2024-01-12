b4d784f6850e39ecf019e624c6e0cdd0ceb1ffa5
nidhi-tripathi <nidhitri120393@gmail.com>
2024-01-12 12:32:20 +0530

    this is payment method

---
+++ server/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const stripe = require("stripe")("sk_test_51NrtfdSDq8tsGW6GMCFXk7Bb0BlRKrt93NaUYe1wN6KZ3uhdwOmD7cviPjJuc1xzpdcvxYqdI2IE4zXztlIdytzL00JiK6UpNO")
+app.use(express.json());
+app.use(cors())
+
+app.post("/api/create-checkout-session",async(req,res)=>{
+    const {products} = req.body;
+
+    const lineItems = products.map((product)=>({
+        price_data:{
+            currency:"inr",
+            product_data:{
+                name:product.dish
+            },
+           unit_amount:product.price * 100, 
+        },
+        quantity:product.qnty
+    }));
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types:["card"],
+        line_items:lineItems,
+        mode:"payment",
+        success_url:"http://localhost:3000/sucess",
+        cancel_url:"http://localhost:3000/cancel",
+    });
+
+    res.json({id:session.id})
+ })
+
+app.listen(7000,()=>{
+    console.log("running")
+})
